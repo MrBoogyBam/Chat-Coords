683c275ccc0f3d1edad725e5da911398e2a8d815
The-head-obamid <66978282+The-head-obamid@users.noreply.github.com>
2022-07-14 00:12:01 -0400

    Update ChatCoordsMixin.java
    no more debug message

--- src/main/java/net/mrboogybam/chatcoords/mixin/ChatCoordsMixin.java
+++ src/main/java/net/mrboogybam/chatcoords/mixin/ChatCoordsMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ChatCoordsMixin {
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
-        ChatCoords.LOGGER.info("This line is printed by an example mod mixin!");
+        // ChatCoords.LOGGER.info("This line is printed by THE chat coords mixin!");
     }
 }
